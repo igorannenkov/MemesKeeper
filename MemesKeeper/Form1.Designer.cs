444e9a83bd239a51d16d202051381c5596604fde
Igor Annenkov <igorannenkov@mail.ru>
2023-05-13 00:56:12 +0300

    Basic functionality added

---
+++ MemesKeeper/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MemesKeeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(889, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 450);
+            this.Controls.Add(this.tabControl1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Хранитель мемчиков";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ MemesKeeper/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MemesKeeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(Environment.CurrentDirectory);
+           
+            foreach (var item in dirInfo.GetDirectories())
+            {
+                TabPage tabPage = new TabPage(item.Name);
+                ListView listView = new ListView();
+                ImageList imageList = new ImageList();
+                string[] pics = Directory.GetFiles(item.Name, "*.JPG");
+                DirectoryInfo innerDirInfo = new DirectoryInfo(item.FullName);
+                FileInfo[] files = innerDirInfo.GetFiles("*.jpg");
+
+                listView.View = View.LargeIcon;                
+                imageList.ImageSize = new Size(128, 128);
+                imageList.ColorDepth = ColorDepth.Depth32Bit;
+                
+                for(int i = 0; i<pics.Length; i++)
+                {
+                    Image image = Image.FromFile(pics[i]);
+                    ListViewItem listViewItem = listView.Items.Add(files[i].Name);
+
+                    imageList.Images.Add(image);
+                    listViewItem.Tag = pics[i];
+                    listViewItem.ImageKey = item.Name;
+                    listViewItem.ImageIndex = i;
+                }
+
+                listView.LargeImageList = imageList;
+                listView.SmallImageList = imageList;
+                listView.Dock = DockStyle.Fill;
+                listView.MouseDoubleClick += ListView_MouseDoubleClick;
+                listView.KeyDown += ListView_KeyDown;
+                tabPage.Controls.Add(listView);
+                tabControl1.TabPages.Add(tabPage);
+
+                listView.BackColor = Color.Black;
+                listView.ForeColor = Color.White;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                tabControl1.Controls.Clear();
+                Form1_Load(sender, e);
+            }
+        }
+
+        private void ListView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                foreach (ListViewItem itm in (sender as ListView).SelectedItems)
+                {
+                    int imgIndex = itm.ImageIndex;
+                    if (imgIndex >= 0 && imgIndex < itm.ImageList.Images.Count)
+                    {
+                        Image image = Image.FromFile(itm.Tag.ToString());
+                        Clipboard.SetImage(image);
+                    }
+                }
+            }
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            foreach (ListViewItem itm in (sender as ListView).SelectedItems)
+            {
+                int imgIndex = itm.ImageIndex;
+                if (imgIndex >= 0 && imgIndex < itm.ImageList.Images.Count)
+                {
+                    Image image = Image.FromFile(itm.Tag.ToString());
+                    Clipboard.SetImage(image);
+                }
+            }
+        }
+    }
+}
